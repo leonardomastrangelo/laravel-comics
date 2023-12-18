2b2e44f3684091b993dcf9f9d9e152de53d3bbc6
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-12-18 14:34:18 +0100

    scaffholding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel base @yield('title', 'template')</title>
+    <title>Laravel Comics - @yield('title', 'template')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,7 +18,7 @@
 <body>
     @include('partials.header')
 
-    yield('content')
+    @yield('content')
 
     @include('partials.footer')
 </body>
