2461017ba452ad2c220b19970eecf4f9edfe2696
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-12-18 14:40:06 +0100

    added db comics

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $products = config('comics.comics');
+    dd($products);
+    return view('home', compact('products'));
 })->name = "home";
